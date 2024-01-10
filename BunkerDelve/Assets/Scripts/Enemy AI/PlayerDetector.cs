691bb78ec136531c5236ca190f434630124808cf
BKhaleque <37214286+BKhaleque@users.noreply.github.com>
2024-01-10 16:45:57 +0000

    Implemented some navmesh stuff for enemy ai

--- BunkerDelve/Assets/Scripts/Enemy AI/PlayerDetector.cs
+++ BunkerDelve/Assets/Scripts/Enemy AI/PlayerDetector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerDetector : MonoBehaviour
 {
@@ -11,16 +12,23 @@ public class PlayerDetector : MonoBehaviour
     public float speed;
     public float respawnInterval;
 
+    private NavMeshAgent agent;
+
     
     private GameObject player;
     private float threatTimer;
     private float respawnTimer;
+    private Vector3 moveTarget;
     
     // Start is called before the first frame update
     void Awake()
     {
+        agent = GetComponent<NavMeshAgent>();
+        speed = agent.speed;
         player = GameObject.FindGameObjectWithTag("Player");
         threatLevel = 0;
+        moveTarget = RandomNavmeshLocation();
+
     }
 
     // Update is called once per frame
@@ -37,17 +45,47 @@ public class PlayerDetector : MonoBehaviour
         //spawn the monster in a certain radius of the player
 
         //pick a point within a radius of 100 - threatLevel from the player
-        var centerOfRadius = player.transform.position;
-        var radius = 100 - threatLevel;
-        var target = centerOfRadius + (Vector3)(radius * Random.insideUnitCircle);
+         var centerOfRadius = player.transform.position;
+         var radius = 100 - threatLevel;
+         var respawnTarget = centerOfRadius + (Vector3)(radius * Random.insideUnitCircle);
+        // //check if the point is within the navmesh
+        // NavMeshHit hit;
+        // while (!NavMesh.SamplePosition(target, out hit, 0, 1))
+        // {
+        //     target = centerOfRadius + (Vector3)(radius * Random.insideUnitCircle);
+        // }
+        //move the monster to the point
+        agent.destination = moveTarget;
 
 
         if (respawnTimer >= respawnInterval)
         {
-            transform.position = target;
+            moveTarget = RandomNavmeshLocation();
+            transform.position = respawnTarget;
             respawnTimer = 0;
+        }
+
+        if (threatLevel >= threatLevelMax)
+        {
+            //TODO
         }
             
         
+    }
+    
+    public Vector3 RandomNavmeshLocation() {
+        var radius = 100 - threatLevel;
+
+        //Vector3 randomDirection = Random.insideUnitSphere * radius;
+        //randomDirection  += transform.position;
+        var centerOfRadius = player.transform.position;
+        var source = centerOfRadius + (Vector3)(radius * Random.insideUnitCircle);
+
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(source, out hit, radius, 1)) {
+            finalPosition = hit.position;            
+        }
+        return finalPosition;
     }
 }
